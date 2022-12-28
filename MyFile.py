7539b83317d0d6d7bae260bf497646c35817a299
Nitish Vig <43816970+nitishvig007@users.noreply.github.com>
2022-12-28 20:56:15 +0530

    New Work from Nitish

---
+++ MyFile.py
@@ -0,0 +1,15 @@
+
+
+x = 11
+print('Even' if x%2 == 0 else "Odd")
+
+
+x = 20
+print('Even' if x%2 == 0 else "Odd")
+
+
+word = "madam"
+print('Palindorme' if word[ : : 1] == word[ : : -1] else "Not a Palindrome")
+
+word = "naMAN"
+print('Palindorme' if word[ : : 1] == word[ : : -1] else "Not a Palindrome")
